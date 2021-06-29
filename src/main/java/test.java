cafd0321c985f36d60dd68179f7bcffc73fce5f2
LiCJ <1603198315@qq.com>
2021-06-29 19:45:58 +0800

    fox commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,5 +6,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("I'm Fix_hot");
     }
 }
